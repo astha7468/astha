0188fc55875f845e4a14a1a85fe5c9b416f75c8b
Astha <rathoreastha12@gmail.com>
2026-02-11 09:58:24 +0530

    update logic

---
+++ dsa/Main.java
@@ -0,0 +1,29 @@
+package dsa;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int result = subtractProductAndSum(n);
+        System.out.println("Result: " + result);
+        
+        sc.close();
+    }
+
+    public static int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+}
